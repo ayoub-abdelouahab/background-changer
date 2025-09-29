7234b2aa3222231f567ca90841f63d6ed71e304d
ayoub-abdelouahab <ayoubabdelouahab29@gmail.com>
2025-09-29 18:03:04 +0100

    pushing the project

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# background-changer
-a responsive website build by html css js that user can chose image of suggest images and it will be the background of the site
+# Responsive Background Changer Website
+
+A simple responsive website built with **HTML**, **CSS**, and **JavaScript**.  
+The user can choose one of the suggested images, and it will be set as the background of the site dynamically.
+
+## Features
+- Responsive design (works on mobile, tablet, desktop).
+- User can select from multiple suggested images.
+- Background of the site changes instantly when an image is selected.
+- Simple and lightweight (no frameworks used).
+
+## Demo
+![screenshot](./screenshot.png)
+
+## Installation
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/username/background-changer.git
---
+++ main.js
@@ -0,0 +1,40 @@
+function soloch() {
+    document.querySelector("body").style.backgroundImage = "url('images/solo.png')";
+    document.querySelector("body").style.backgroundSize = "cover";
+    document.querySelector("body").style.backgroundPosition = "center";
+    document.querySelector(" div").style.display = "none"; // Hides the solo div
+}
+function shadwo() {
+    document.querySelector("body").style.backgroundImage = "url('images/shadow.jpg')";
+    document.querySelector("body").style.backgroundSize = "cover";
+    document.querySelector("body").style.backgroundPosition = "center";
+    document.querySelector(" div").style.display = "none"; // Hides the solo div
+}
+
+function rudi() {
+    document.querySelector("body").style.backgroundImage = "url('images/rudii.jpg')";
+    document.querySelector("body").style.backgroundSize = "cover";
+    document.querySelector("body").style.backgroundPosition = "center";
+    document.querySelector(" div").style.display = "none"; // Hides the solo div
+}
+
+function tbate() {
+    document.querySelector("body").style.backgroundImage = "url('images/tbate.jpg')";
+    document.querySelector("body").style.backgroundSize = "cover";
+    document.querySelector("body").style.backgroundPosition = "center";
+    document.querySelector(" div").style.display = "none"; // Hides the solo div
+}
+
+function note() {
+    document.querySelector("body").style.backgroundImage = "url('images/inf.jpg')";
+    document.querySelector("body").style.backgroundSize = "cover";
+    document.querySelector("body").style.backgroundPosition = "top";
+    document.querySelector(" div").style.display = "none"; // Hides the solo div
+}
+
+function shin() {
+    document.querySelector("body").style.backgroundImage = "url('images/86.jpg')";
+    document.querySelector("body").style.backgroundSize = "cover";
+    document.querySelector("body").style.backgroundPosition = "center";
+    document.querySelector(" div").style.display = "none"; // Hides the solo div
+}
